f8333d51976a761e25ecb01b9bdabe86c30f7356
“Chica <142940971+Dj-messup@users.noreply.github.com>
2025-05-31 15:28:17 -0600

    Add HelloWorld console project

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HelloWorld
+{
+    public class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+//Console.WriteLine("Hello, World!");
