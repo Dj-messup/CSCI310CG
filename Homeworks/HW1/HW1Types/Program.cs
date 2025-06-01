81e743c42a334a39e795330a98baa618bdc5afa9
“Chica <142940971+Dj-messup@users.noreply.github.com>
2025-06-01 10:17:33 -0600

    just words for me to go back on

--- Homeworks/HW1/HW1Types/Program.cs
+++ Homeworks/HW1/HW1Types/Program.cs
@@ -11,7 +11,7 @@ namespace HW1Types
             int islandCount = 137; // Total Hawaiian islands according to google
             decimal locoMocoPrice = 10.00m; //If no m then considered a double. Didn't know if good for assign.
             string homeBase = "Oʻahu";
-            char placeInitial = 'O'; // char
+            char placeInitial = 'O';
 
             Console.WriteLine($"There's {islandCount} islands in Hawai'i in general."); //use WriteLine for.
             Console.WriteLine($"A loco moco usually costs around ${locoMocoPrice}."); //new line
@@ -24,13 +24,13 @@ namespace HW1Types
             homeBase = "Colorado";
             placeInitial = 'C'; //change for sake of assignment
             
-            Console.WriteLine($"\nBut, there is only {islandCount} main islands.");
+            Console.WriteLine($"\nBut, there is only {islandCount} main islands.");//add space and design
             Console.WriteLine($"I found a loco moco for ${locoMocoPrice} once.");
             Console.WriteLine($"I’m now staying in {homeBase} unfortunately.");
             Console.WriteLine($"The first initial of Colorado is {placeInitial}.");
             
             //calculate the area of a triangle using herons
-            Console.WriteLine("now lets calculate the area of a triangle using heron's formula.");
+            Console.WriteLine("lets calculate the area of a triangle using heron's formula.");
             double a = 3.0;
             double b = 4.0;
             double c = 5.0; // right triangle values
@@ -41,10 +41,11 @@ namespace HW1Types
             // Heron's formula
             double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
 
-            Console.WriteLine("\nUsing sides a=3, b=4, c=5:");
+            Console.WriteLine("\nUsing sides a=3, b=4, c=5:");//add readabily/clarity w/new line
             Console.WriteLine($"and knowing half of the perimeter is = {s}");
             Console.WriteLine("\narea = square root of (s (s−a)(s−b)(s−c))");
             Console.WriteLine($"The area of the triangle is {area}");
+            //some of these seem like a lot of fluff of words, but they are descriptions soooo 
         }
     }
 }
