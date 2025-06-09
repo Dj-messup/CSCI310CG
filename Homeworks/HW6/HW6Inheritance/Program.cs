ec8e4f2a4c68587f6b749672bacfd13fe06f7e99
“Chica <142940971+Dj-messup@users.noreply.github.com>
2025-06-08 23:49:36 -0600

    hw 6 done?

--- Homeworks/HW6/HW6Inheritance/Program.cs
+++ Homeworks/HW6/HW6Inheritance/Program.cs
@@ -7,7 +7,7 @@ namespace HW6Inheritance
     // Base class
     public class People
     {
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty; // had a warning, Non-nullable property
     }
 
     // Derived class #1
@@ -15,7 +15,7 @@ namespace HW6Inheritance
     {
         public void Study()
         {
-            Console.WriteLine($"{Name} is studying hard for the exams.");
+            Console.WriteLine($"{Name} is studying hard for the exams."); // not like i have three jobs
         }
     }
 
@@ -27,14 +27,14 @@ namespace HW6Inheritance
             Console.WriteLine($"{Name} is teaching a fascinating lesson.");
         }
     }
-    public class UniversityDemo   // better name than “Program”
+    public class UniversityDemo   //can always change
     {
         public static void Main()
         {
             var student = new Student { Name = "Chica" };
             student.Study();
 
-            var prof = new Professor { Name = "Professor Alayami" };
+            var prof = new Professor { Name = "Professor Alayami" }; //yuh cisb
             prof.Teach();
         }
     }
